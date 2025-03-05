75e62dcadaac808aa2640c7c47c9452922b10831
Guilherme Lessa <gui.silva.lessa@gmail.com>
2025-03-05 17:13:48 -0300

    att 4

--- js/personalizado.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(window).on("scroll", function () {
-      if ($(window).scrollTop() > 100) {
-          $(".menu").addClass("menu-scroll");
-      } else {
-          $(".menu").removeClass("menu-scroll");
-      }
-  });
-});
